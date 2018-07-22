1879244cabafe1f2d7f7f91b323d66aff247f801
AjitSingh <jeetsingh.ajit@gmail.com>
2018-07-22 19:59:18 +0530

    Fixes storage

--- lib/sidekiq_queue_metrics/storage.rb
+++ lib/sidekiq_queue_metrics/storage.rb
@@ -2,11 +2,15 @@ module Sidekiq::QueueMetrics
   class Storage
     class << self
       def set_stats(key = stats_key, value)
-        Sidekiq.redis_pool.set(key, value)
+        Sidekiq.redis_pool.with do |conn|
+          conn.set(key, value)
+        end
       end
 
       def get_stats(key = stats_key)
-        Sidekiq.redis_pool.get(key)
+        Sidekiq.redis_pool.with do |conn|
+          conn.get(key)
+        end
       end
 
       def stats_key
