f61684475e3c119a4e3cf03a9bbebf04d26e18b4
AjitSingh <jeetsingh.ajit@gmail.com>
2018-09-16 10:23:45 +0530

    Adds link to failed queue and correct urls

--- lib/sidekiq_queue_metrics/web_extension.rb
+++ lib/sidekiq_queue_metrics/web_extension.rb
@@ -8,11 +8,19 @@ module Sidekiq::QueueMetrics
         render(:erb, File.read(File.join(view_path, "queues_stats.erb")))
       end
 
-      app.get '/queue/:queue_name/summary' do
-        @queue = route_params[:queue_name]
+      app.get '/queues/:queue/summary' do
+        @queue = route_params[:queue]
         @queue_stats = Sidekiq::QueueMetrics.fetch[@queue]
         @failed_jobs = Sidekiq::QueueMetrics.failed_jobs(@queue)
         render(:erb, File.read(File.join(view_path, "queue_summary.erb")))
+      end
+
+      app.get '/queues/:queue/jobs/:jid' do
+        queue = route_params[:queue]
+        jid = route_params[:jid]
+        failed_jobs = Sidekiq::QueueMetrics.failed_jobs(queue)
+        @job = failed_jobs.find {|job| job['jid'] == jid}
+        render(:erb, File.read(File.join(view_path, "failed_job.erb")))
       end
     end
   end
