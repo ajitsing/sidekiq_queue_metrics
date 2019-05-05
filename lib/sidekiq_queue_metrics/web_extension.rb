e201dbbd149b27d273a8ee9a5d866579bb8ec33f
AjitSingh <jeetsingh.ajit@gmail.com>
2019-05-05 11:24:22 +0530

    Added blinking behaviour whenever failed or success count changes

--- lib/sidekiq_queue_metrics/web_extension.rb
+++ lib/sidekiq_queue_metrics/web_extension.rb
@@ -4,7 +4,18 @@ module Sidekiq::QueueMetrics
       view_path = File.join(File.expand_path("..", __FILE__), "views")
 
       app.get "/queue_metrics" do
-        @queue_metrics = Sidekiq::QueueMetrics.fetch
+        queue_metrics = Sidekiq::QueueMetrics.fetch
+        @@last_metrics ||= queue_metrics
+
+        @queue_metrics = queue_metrics.each_with_object({}) do |queue_metric, new_queue_metrics|
+          queue, metric = queue_metric
+          new_queue_metrics[queue] = metric.each_with_object({}) do |current_metric, updated_metrics|
+            name, count = current_metric
+            updated_metrics[name] = {'count' => count, 'animate' => @@last_metrics[queue][name] != count}
+          end
+        end
+
+        @@last_metrics = queue_metrics
         render(:erb, File.read(File.join(view_path, "queues_stats.erb")))
       end
 
